965a37fc222700bea98c50b94b217f01b018bf97
whyyor <whyyor@gmail.com>
2022-05-08 13:21:44 +0530

    add connectButton via rainbow connect

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,52 @@
 import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+    apiProvider,
+    configureChains,
+    getDefaultWallets,
+    RainbowKitProvider,
+    darkTheme
+} from '@rainbow-me/rainbowkit';
+import {
+    chain,
+    createClient,
+    WagmiProvider
+} from 'wagmi';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const {
+    chains,
+    provider
+} = configureChains(
+    [chain.mainnet, chain.rinkeby],
+    [
+        apiProvider.infura('d1b36e5129b4479f842a7d30ad57c892'),
+        apiProvider.fallback()
+    ]
+);
+const {
+    connectors
+} = getDefaultWallets({
+    appName: 'My RainbowKit App',
+    chains
+});
+const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+})
+
+
+function MyApp({
+    Component,
+    pageProps
+}) {
+    return (
+        <WagmiProvider client={wagmiClient}>
+            <RainbowKitProvider chains={chains} theme={darkTheme()}>
+                <Component {...pageProps}/>
+            </RainbowKitProvider>
+        </WagmiProvider>
+  );
 }
 
 export default MyApp
